2d2cfdbdfbf819d5afde2ef39e862188c1eab7b7
GitoFox <55304324+GitoFox@users.noreply.github.com>
2023-05-31 12:55:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/encuestadores/:rut', (req, res) => {
           imagenPath = 'img/Saludando.png';
         }
 
-        imagenURL = 'http://3.83.2.236:3000/img/Saludando.png' + path.basename(imagenPath); // Obtén solo el nombre del archivo de la imagen
+        imagenURL = 'http://3.83.2.236:3000/img/' + path.basename(imagenPath); // Obtén solo el nombre del archivo de la imagen
         encuestador.imagenURL = imagenURL;
 
         // Leer y procesar los proyectos del encuestador
